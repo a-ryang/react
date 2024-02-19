e63b538acb11bca3613a599a6018cb5c616c613b
a-ryang <chnwu.kim@gmail.com>
2024-02-19 09:03:35 +0900

    advanced state management
    * Context
    * useReducer

---
+++ 10-advanced-state-management/src/store/shoppring-card-context.jsx
@@ -0,0 +1,140 @@
+import { createContext, useState, useReducer } from "react";
+
+import { DUMMY_PRODUCTS } from "../dummy-products";
+
+export const CartContext = createContext({
+  items: [],
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
+});
+
+// 밖에 생성한 이유:
+// 1. 재생성 방지
+// 2. 함수에서 정의 또는 업데이트 되는 그 어떤 값에도 직접적인 접근을 필요로 하지 않기 떄문
+function shoppinrgCartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  return state;
+}
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppinrgCartReducer,
+    {
+      items: [],
+    }
+  );
+
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      type: "ADD_ITEM",
+      payload: id,
+    });
+    // setShoppingCart((prevShoppingCart) => {
+    //   const updatedItems = [...prevShoppingCart.items];
+
+    //   const existingCartItemIndex = updatedItems.findIndex(
+    //     (cartItem) => cartItem.id === id
+    //   );
+    //   const existingCartItem = updatedItems[existingCartItemIndex];
+
+    //   if (existingCartItem) {
+    //     const updatedItem = {
+    //       ...existingCartItem,
+    //       quantity: existingCartItem.quantity + 1,
+    //     };
+    //     updatedItems[existingCartItemIndex] = updatedItem;
+    //   } else {
+    //     const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+    //     updatedItems.push({
+    //       id: id,
+    //       name: product.title,
+    //       price: product.price,
+    //       quantity: 1,
+    //     });
+    //   }
+
+    //   return {
+    //     items: updatedItems,
+    //   };
+    // });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: {
+        productId,
+        amount,
+      },
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCartState.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
